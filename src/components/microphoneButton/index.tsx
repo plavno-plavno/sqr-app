c03303c0ab69941021048d2ade8cee541383553b
bezirgen8989 <bezirgen8989@mail.ru>
2025-05-28 12:59:17 +0300

    фикс VAD 7.1

--- src/components/microphoneButton/index.tsx
+++ src/components/microphoneButton/index.tsx
@@ -53,7 +53,9 @@ export const MicrophoneButton = ({
         audio: {
           echoCancellation: true,
           noiseSuppression: true,
-          autoGainControl: true
+          autoGainControl: true,
+          channelCount: 1, // Используем только один канал
+          sampleRate: 16000 // Устанавливаем частоту дискретизации
         }
       });
 
