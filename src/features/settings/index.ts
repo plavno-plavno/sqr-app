b8bf3c6e8c1de633b5b4367365c260ede481584c
ptrkorzh <ptrkorzh@gmail.com>
2025-09-15 15:01:29 +0300

    Deployment trigger

--- src/features/settings/index.ts
+++ src/features/settings/index.ts
@@ -1,6 +1,6 @@
 // Settings
+export { settingsMock } from "./model/settings";
+export type { SettingsItemType } from "./model/settings";
+export { useSettingsStore } from "./model/settings-store";
 export { SettingsItem } from "./ui/settings-item";
 export { SettingsSwitchItem } from "./ui/settings-switch-item";
-export type { SettingsItemType } from "./model/settings";
-export { settingsMock } from "./model/settings";
-export { useSettingsStore } from "./model/settings-store";
